14baaf087bc43e47d30fd583ac4be3fb67678224
caleb <calebsg2253284+git@gmail.com>
2024-10-14 23:24:57 -0400

    lay out possible functions to add

--- scripts/added/added.js
+++ scripts/added/added.js
@@ -1,8 +1,25 @@
-// push/pull function to/from localstorage
+// push function to localstorage
 const pushLocalStorage = (key, data) => {
   localStorage.setItem(key, data.toString());
 }
 
-// startup function
+// pull function from localstorage
+// returns the runnable function
+const pullLocalStorage = (key) => {
+  const rawFunc = localStorage.getItem(key);
+  return eval('(' + rawFunc + ')');
+}
 
-// 
+// startup function
+const startup = () => {}
+
+// add listener to existing orders
+const catchUp = () => {}
+
+// add listener to any new orders
+const addNewOrder = () => {}
+
+// toggle to disable if needed
+const toggleDisableAgg = () => {}
+
+// each order has a button to agg manually
