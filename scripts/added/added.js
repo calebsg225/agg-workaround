ad365966444a04f89703c315013f736cbbded4d1
caleb <calebsg2253284@gmail.com>
2024-10-19 10:44:28 -0400

    work on startup function

--- scripts/added/added.js
+++ scripts/added/added.js
@@ -13,16 +13,71 @@ const pullLocalStorage = (key) => {
 // startup function
 const startup = () => {
   // add toggle to dispatch
-  const header = document.getElementById('dHeader');
+  const header = document.getElementById('header');
+  header.style.position = 'relative'
 
+  const toggle = document.createElement('input');
+  toggle.type = 'checkbox';
+  toggle.className = 'toggle-add-workaround';
+
+  header.append(toggle);
+
+  // delete errors and whatnot
+  localStorage.removeItem('dispatchOne');
+  localStorage.removeItem('dispatchTwo');
+
+  // add set to localStorage
+  localStorage.setItem('ddorders', JSON.stringify([]));
+
+  // monkey patch main functions
+  const tempCallAgg = window.callAggregatorWithPw;
+  const tempCancelAgg = window.cancelAggregator;
+
+  window.callAggregatorWithPw = (orderId) => {
+    const ddOrders = new Set(eval(localStorage.getItem('ddorders')));
+    if (!(ddOrders.has(+orderId))) {
+      ddOrders.add(+orderId);
+    }
+    localStorage.setItem('ddorders', JSON.stringify([...ddOrders]));
+    tempCallAgg(orderId);
+  }
+
+  window.cancelAggregator = (orderId, aggCarrierId) => {
+    const ddOrders = new Set(eval(localStorage.getItem('ddorders')));
+    if (ddOrders.has(+orderId)) {
+      ddOrders.delete(+orderId);
+    }
+    localStorage.setItem('ddorders', JSON.stringify([...ddOrders]));
+    tempCancelAgg(orderId, aggCarrierId);
+  }
+
+  // create agg blocker interval
   const interval = window.setInterval(() => {
-    const orders = document.getElementsByClassName('order-container');
+    const ordersAssign = document.getElementById('order-assign');
+    for (const order of ordersAssign.childNodes) {
+      if (new Set(eval(localStorage.getItem('ddorders'))).has(+order.childNodes[1].dataset.id)) { return }
+      if (order.childNodes[1].childElementCount >= 4 && new Set(['W', 'w']).has(order.childNodes[1].childNodes[1].innerText.charAt(0))) {
+
+      }
+    }
     for (let i = 0; i < orders.length; i++) {
-      if (orders[i].childNodes[1].innerText === 'waiting for agg...') {
+      if (new Set(['W', 'w']).has(orders[i].childNodes[1].innerText.charAt(0))) {
         orders[i].childNodes[1].innerText = 'agg canceled';
       }
     }
   }, 2000);
+}
+
+const DDDchangeStatus = (id) => {
+  const orderContainer = document.getElementById('order-assign');
+  for(const order of orderContainer.childNodes) {
+    if (+order.childNodes[1].dataset.id === id) {
+      const newStatus = document.createElement('div');
+      newStatus.className = 'newStatusTitle';
+      newStatus.innerText = 'Waiting for agg...';
+      order.childNodes[1].childNodes[0].after(newStatus);
+    }
+  }
 }
 
 
--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,11 @@ import header from "./default/header.js";
 import main from "./default/main.js";
 
 helper.appendElements([header, main], document.body);
+
+const cancelAggregator = (orderId, aggCarrierId) => {
+  console.log('canceling', orderId);
+}
+
+const callAggregatorWithPw = (orderId) => {
+  console.log('agging', orderId);
+}
